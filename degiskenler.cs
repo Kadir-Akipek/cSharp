be519b7015e5fa863e91716582de76e240333e7c
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-08-19 15:49:43 +0300

    cSharp

---
+++ degiskenler.cs
@@ -0,0 +1,21 @@
+/*değişkenlerde büyük-küçük harf duyarlılığı mevuttur
+fiyat, Fiyat farklı değişkenlerdir*/
+
+var ogrenciAdi = "Kadir ";
+var ogranciSoyadi = "Akipek";
+var adSoyad = ogrenciAdi + ogranciSoyadi;
+var ogrenciNo = "1025";
+var ogrenciTc = "111111";
+var ogrenciDogumYili = 2005;
+var ogrenciYas = 2024 - ogrenciDogumYili;
+
+Console.WriteLine(adSoyad);
+Console.WriteLine(ogrenciNo);
+Console.WriteLine(ogrenciYas);
+Console.WriteLine(ogrenciTc);
+
+var urun1_fiyat = 50;
+var urun2_fiyat = 60.5;
+
+var toplam = urun1_fiyat + urun2_fiyat;
+Console.WriteLine(toplam);
